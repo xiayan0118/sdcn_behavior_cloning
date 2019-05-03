43fbe09401e58e2f621756729194314f535c7a03
Yan Xia <drseanxy@mac.com>
2019-05-03 05:58:00 -0700

    final version

--- model.py
+++ model.py
@@ -6,24 +6,19 @@ from random import shuffle
 import numpy as np
 import sklearn
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Cropping2D, Dense, Dropout, Flatten, Lambda
+from keras.layers.convolutional import Conv2D
 from keras.models import Sequential
 from scipy import ndimage
 from sklearn.model_selection import train_test_split
 
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPool2D
+# ------ Hyperparameters ------
 
-# Use generator and add comments
-# Train/validation/test
-# Adam
-# Shuffle before splitting
-# http://alexlenail.me/NN-SVG/LeNet.html
-
-# Hyperparameters
-BATCH_SIZE = 85
+# Each line in CSV is converted to 6 training instances. So batch size = 64 / 6 ~= 11
+BATCH_SIZE = 11
 ROW, COL, CH = 160, 320, 3
 NUM_EPOCHS = 50
+# Corrections of angles from center, left and right
 CORRECTIONS = [0.0, 0.2, -0.2]
 
 def load_images():
@@ -34,6 +29,7 @@ def load_images():
     for line in reader:
       samples.append(line)
 
+  # Shuffle examples
   shuffle(samples)
   train_samples, val_samples = train_test_split(samples, test_size=0.2)
   return train_samples, val_samples
@@ -47,6 +43,8 @@ def generator(samples, batch_size=32):
 
       imgs = []
       angles = []
+
+      # Each line in CSV is converted to 6 training instances: center, left, right and their flipped version
       for batch_sample in batch_samples:
         for i in range(3):
           img_name = './data/' + batch_sample[i].strip()
@@ -71,17 +69,13 @@ def build_model():
   model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
 
   model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
-# model.add(MaxPool2D())
   model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
-  # model.add(MaxPool2D())
   model.add(Conv2D(64, (3, 3), activation='relu'))
-  # model.add(MaxPool2D())
-  model.add(Conv2D(64, (3, 3), activation='relu'))  
-  # model.add(MaxPool2D())
+  model.add(Conv2D(64, (3, 3), activation='relu'))
   model.add(Flatten())
   model.add(Dropout(0.7))
   model.add(Dense(100))
-#   model.add(Dropout(0.7))    
+  model.add(Dropout(0.7))
   model.add(Dense(50))
   model.add(Dense(10))
   model.add(Dense(1))
@@ -89,6 +83,7 @@ def build_model():
 
 
 if __name__ == "__main__":
+  # Load examples from csv file and make train/validation generators
   train_samples, val_samples = load_images()
   train_gen = generator(train_samples, BATCH_SIZE)
   val_gen = generator(val_samples, BATCH_SIZE)
@@ -96,6 +91,7 @@ if __name__ == "__main__":
   model = build_model()
   model.compile(loss='mse', optimizer='adam')
 
+  # Callbacks that facilitate optimization
   early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=0)
   checkpoint = ModelCheckpoint('model-{epoch:03d}-{val_loss:03f}.h5',
                                monitor='val_loss',
@@ -105,16 +101,16 @@ if __name__ == "__main__":
                                 patience=2, min_lr=0.000001, verbose=1)
 
   history_object= model.fit_generator(generator=train_gen,
-                      steps_per_epoch=ceil(len(train_samples)/BATCH_SIZE),
-                      validation_data=val_gen,
-                      validation_steps=ceil(len(val_samples)/BATCH_SIZE),
-                      epochs=NUM_EPOCHS,
-                      callbacks=[early_stopping, checkpoint, reduce_lr],
-                      verbose=1)
+                                      steps_per_epoch=ceil(len(train_samples)/BATCH_SIZE),
+                                      validation_data=val_gen,
+                                      validation_steps=ceil(len(val_samples)/BATCH_SIZE),
+                                      epochs=NUM_EPOCHS,
+                                      callbacks=[early_stopping, checkpoint, reduce_lr],
+                                      verbose=1)
 
   # Save model
-  model.save("model_bs.h5")
+  model.save("model.h5")
 
-  # Save training/validation losses
-  with open('history_bs.p', 'wb') as f:
+  # Save training/validation losses for analysis
+  with open('history.p', 'wb') as f:
     pickle.dump(history_object.history, f)
--- writeup.md
+++ writeup.md
@@ -127,3 +127,5 @@ After the collection process, I had X number of data points. I then preprocessed
 I finally randomly shuffled the data set and put Y% of the data into a validation set. 
 
 I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was Z as evidenced by ... I used an adam optimizer so that manually training the learning rate wasn't necessary.
+
+model_1_dropout_lr-0.013697.h5
